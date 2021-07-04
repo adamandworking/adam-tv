f22d7413e0c63bfa7fbc07c84f12f5c1d4dc914e
adamandworking <adamandworking@gmail.com>
2021-07-04 17:46:05 +0800

    Done chat feature

--- server/index.js
+++ server/index.js
@@ -4,24 +4,18 @@ const io = require('socket.io')(server, { cors: { origin: "*" } })
 var users = {}
 io.on('connection', socket => {
     console.log(socket.id)
-    socket.on('message', ({ name, message }) => {
-        console.log('recevied message:')
-        console.log('name: ' + name)
-        console.log('message: ' + message)
-        name = name + 'FromServer'
-        io.emit('message', { name, message })
-    })
 
     socket.on("Chat:NewUserRegister", ({ id, userName }) => {
         users[id] = userName
-        io.emit("Chat:NewUserRegister", userName)
         io.emit('Chat:UsersChange', Object.values(users))
+    })
+
+    socket.on('Chat:NewMessage', ({ name, message }) => {
+        io.emit('Chat:NewMessage', { name, message })
     })
 
     socket.on('disconnect', function () {
         delete users[socket.id]
-        console.log("Chat Disconnect: users")
-        console.log(users)
         io.emit('Chat:UsersChange', Object.values(users))
     });
 })
--- web/src/components/Chatbox.jsx
+++ web/src/components/Chatbox.jsx
@@ -1,39 +1,78 @@
 import React, { useEffect, useRef, useState } from "react"
 
 const Chatbox = ({ socket }) => {
+  const [userName, setUserName] = useState("Guest")
   const [chatLog, setChatLog] = useState([])
   const [users, setUsers] = useState([])
+  const [chatToSend, setChatToSend] = useState("")
   useEffect(() => {
     socket.on("connect", () => {
-      console.log(socket.id)
-      socket.on("Chat:NewUserRegister", (name) => {
-        console.log('Another guy connected: ' + name)
-      })
-
-      socket.on("message", ({ name, message }) => {
-        console.log(name + ': ' + message)
+      socket.on("Chat:NewMessage", ({ name, message }) => {
+        setChatLog(prevState => {
+          return [...prevState, name + ": " + message]
+        })
       })
 
       socket.on("Chat:UsersChange", (users) => {
-        console.log(users)
         setUsers(users)
       })
       socket.emit("Chat:NewUserRegister", { id: socket.id, userName: socket.id })
     });
   }, [])
 
-  // useEffect(() => {
-  //   setUsersList(users.map((user) => <li key={user}>{user}</li>))
-  // }, [users])
+  const handleUserNameOnChange = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const handleChatToSendOnChange = (e) => {
+    setChatToSend(e.target.value)
+  }
+
+  const handleChatSubmit = (event) => {
+    socket.emit("Chat:NewMessage", { name: userName, message: chatToSend })
+    setChatToSend("")
+    event.preventDefault();
+  }
+
+  // const ChatInputField = () => {
+  //   return (
+  //     <form onSubmit={handleChatSubmit}>
+  //       <textarea value={chatToSend} onChange={handleChatToSendOnChange}></textarea>
+  //     </form>
+  //   )
+  // }
 
   let usersList = users.map((user) => <li key={user}>{user}</li>)
 
   return (
     <div className="h-full w-3/12 bg-gray-300 flex-none">
-      Chatbox
-      <ul>
-        {usersList}
-      </ul>
+      <div>
+        Chatbox
+      </div>
+      <div>
+        <ul>
+          {usersList}
+        </ul>
+      </div>
+      {/* <ChatInputField /> */}
+      <div>
+        {
+          chatLog.map(
+            (ele) => <div>{ele}</div>
+          )
+        }
+      </div>
+      <form onSubmit={handleChatSubmit}>
+        <label>
+          Name:
+          <input onChange={handleUserNameOnChange} />
+        </label>
+        <label>
+          Message:
+          <textarea value={chatToSend} onChange={handleChatToSendOnChange}></textarea>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
